99166a0ff69ccac1714b37361c6c6c35259704b5
Brij Kishor <brizkishor.azad@gmail.com>
2026-05-23 01:08:49 +0530

    Added send otp route

--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -1,5 +1,9 @@
 import express from "express";
-import { login, register, sendOtp } from "../controllers/authController.js";
+import {
+   login,
+   register,
+   sendOtp
+} from "../controllers/authController.js";
 
 const authRouter = express.Router();
 
