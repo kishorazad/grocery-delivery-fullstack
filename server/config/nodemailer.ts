cbc4b0e611bca6c096740bb592470e6ca237c518
Brij Kishor <brizkishor.azad@gmail.com>
2026-05-23 20:41:45 +0530

    fixed nodemailer transporter

--- server/config/nodemailer.ts
+++ server/config/nodemailer.ts
@@ -2,13 +2,19 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
-    port: 587,
+
+    port: 2525,
+
     secure: false,
 
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
     },
+
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
 
     tls: {
         rejectUnauthorized: false,
