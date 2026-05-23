3307ea46daec02b4c78c6c643f370cdb160fa58b
Brij Kishor <brizkishor.azad@gmail.com>
2026-05-23 11:45:13 +0530

    real otp smetp

--- server/config/nodemailer.ts
+++ server/config/nodemailer.ts
@@ -2,21 +2,57 @@ import { createTransport } from "nodemailer";
 
 // Create a transporter using SMTP
 const transporter = createTransport({
+
     host: "smtp-relay.brevo.com",
-    port: 587,
+
+    port: 465,
+
+    secure: true,
+
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
     },
+
 });
 
-const sendEmail = async ({ to, subject, body }: { to: string; subject: string; body: string }) => {
+// Verify SMTP connection
+transporter.verify((error, success) => {
+
+    if (error) {
+
+        console.log("SMTP ERROR:", error);
+
+    } else {
+
+        console.log("SMTP CONNECTED");
+
+    }
+
+});
+
+const sendEmail = async ({
+    to,
+    subject,
+    body
+}: {
+    to: string;
+    subject: string;
+    body: string;
+}) => {
+
     const response = await transporter.sendMail({
+
         from: process.env.SENDER_EMAIL,
+
         to,
+
         subject,
+
         html: body,
+
     });
+
     return response;
 };
 
