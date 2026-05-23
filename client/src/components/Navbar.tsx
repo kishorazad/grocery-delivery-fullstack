b959dda14e1186d3b56173c5dd88e57b667998be
Brij Kishor <brizkishor.azad@gmail.com>
2026-05-24 01:07:32 +0530

    Fixed Herdo eimpordts

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -46,7 +46,7 @@ const Navbar = () => {
                     <form onSubmit={handleSearch} className="hidden sm:flex flex-1 max-w-sm text-xs sm:text-sm">
                         <div className="relative w-full">
                             <SearchIcon className="absolute left-2.5 top-1/2 -translate-y-1/2 size-4 text-zinc-500" />
-                            <input type="text" placeholder="Search for groceries..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} className="w-full pl-8 p-2 bg-orange-50 rounded-full ring ring-app-orange/15 focus:ring-app-orange/30" />
+                            <input type="text" placeholder="Search for medicine..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} className="w-full pl-8 p-2 bg-orange-50 rounded-full ring ring-app-orange/15 focus:ring-app-orange/30" />
                         </div>
                     </form>
 
