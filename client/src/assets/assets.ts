79730d698ee2f1941fc3172f0437d04e09357133
Brij Kishor <brizkishor.azad@gmail.com>
2026-05-24 00:38:26 +0530

    Updatedfooterddod section

--- client/src/assets/assets.ts
+++ client/src/assets/assets.ts
@@ -337,3 +337,26 @@ export const dummyAddressData = [
     { label: "Home", address: "123 Main St ", city: "New York ", state: "NY", zip: "10001", isDefault: true, lat: 40.7128, lng: -74.006, id: "69d3652df9a340288f1a0f8c" },
     { label: "Work", address: "456 Market St ", city: "New York ", state: "NY", zip: "10002", isDefault: false, lat: 40.7128, lng: -74.006, id: "69d3652df9a340288f1a0f8d" },
 ];
+
+export const heroBanners = [
+    {
+        id: 1,
+        title: "Banner 1",
+        image: "/client/src/assets/banner1.png",
+    },
+    {
+        id: 2,
+        title: "Banner 2",
+        image: "/client/src/assets/banner2.png",
+    },
+    {
+        id: 3,
+        title: "Banner 3",
+        image: "/client/src/assets/banner3.png",
+    },
+    {
+        id: 4,
+        title: "Banner 4",
+        image: "/client/src/assets/banner4.png",
+    },
+];
