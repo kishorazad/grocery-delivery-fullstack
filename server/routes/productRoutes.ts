f81146284047f9ecc824f33d26a9423d0b251d34
Brij Kishor <brizkishor.azad@gmail.com>
2026-05-24 23:21:55 +0530

    converted products to slugb architecture

--- server/routes/productRoutes.ts
+++ server/routes/productRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createProduct, deleteProduct, getFlashDeals, getProduct, getProductBySlug, getProducts, updateProduct } from "../controllers/productController.js";
+import { createProduct, deleteProduct, getFlashDeals, getProductBySlug, getProducts, updateProduct } from "../controllers/productController.js";
 import auth from "../middleware/auth.js";
 import admin from "../middleware/admin.js";
 
@@ -7,7 +7,7 @@ const productRouter = express.Router();
 
 productRouter.get("/flash-deals", getFlashDeals);
 productRouter.get("/", getProducts);
-productRouter.get("/:id", getProduct);
+
 productRouter.post("/", auth, admin, createProduct);
 productRouter.put("/:id", auth, admin, updateProduct);
 productRouter.get("/:slug", getProductBySlug);
