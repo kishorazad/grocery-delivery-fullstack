c04c4382d7b11392b73235438abf954f87881223
Brij Kishor <brizkishor.azad@gmail.com>
2026-05-31 20:26:18 +0530

    fix notifica tion blocks

--- client/public/firebase-messaging-sw.js
+++ client/public/firebase-messaging-sw.js
@@ -7,31 +7,36 @@ importScripts(
 );
 
 firebase.initializeApp({
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID",
 });
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
+messaging.onBackgroundMessage(function(payload) {
 
     console.log(
-        "Background Message:",
+        "[firebase-messaging-sw.js] Received background message ",
         payload
     );
 
-    self.registration.showNotification(
-        payload.notification.title,
-        {
-            body:
-                payload.notification.body,
+    const notificationTitle =
+        payload.notification.title;
 
-            icon:
-                "/logo.png",
-        }
+    const notificationOptions = {
+        body:
+            payload.notification.body,
+
+        icon:
+            "/logo.png",
+    };
+
+    self.registration.showNotification(
+        notificationTitle,
+        notificationOptions
     );
 });
